658bfaced836eecb7fefc2434f45b487bc13fbdc
xininny <xininny@gmail.com>
2024-08-16 11:43:35 +0900

    Study

---
+++ 시간순삭 파이썬/2장/Practice02-01.py
@@ -0,0 +1,5 @@
+num1 = int(input("첫 번째 숫자를 입력하시오: "))
+num2 = int(input("두 번째 숫자를 입력하시오: "))
+num3 = int(input("세 번째 숫자를 입력하시오: "))
+average = (num1+num2+num3)/3
+print(num1, num2, num3,"의 평균은 ", average,"입니다.")
